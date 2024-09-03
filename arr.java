47bd1595da5d4c2edbcf63646452a6875ba6001d
Balu9911 <sbalu2401@gmail.com>
2024-09-03 23:26:09 +0530

    https://github.com/Balu9911/java.git

---
+++ arr.java
@@ -0,0 +1,58 @@
+class R{
+    public static void main(String[] args) {
+        int []a ;
+        a=new int [4];
+        a[0]=100;
+        a[1]=200;
+        a[3]=300;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[3]);
+        System.out.println(a);
+    }
+ }
+
+ class T{
+    public static void main(String[] args) {
+        int[] a;
+        a=new int[4];
+        String[] enames;
+        enames=new String[4];
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[3]);
+        System.out.println(enames[1]);
+    }
+ }
+
+ class Q{
+    public static void main(String[] args) {
+        int[] eids={101,102,103};
+        String[] enames={"balu","harish","ravi"};
+
+        System.out.println(eids[0]);
+        System.out.println(eids[1]);
+        System.out.println(eids[2]);
+        System.out.println(enames[0]);
+    }
+ }
+
+ //class X{
+ //   public static void main(String[] args) {
+ //       int[] eids={101,102,103,104};
+ //       String[] enames={"balu","harush","jaga"};
+
+ //       for (String name:enames);
+  //      System.out.println(ename);
+        
+  //  }
+ //}
+
+ class L{
+    int a=100;
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+    
+ }
--- you.java
+++ you.java
@@ -8,10 +8,56 @@ class Z{
         Z t2=new Z();
         t2.b=20;
         System.out.println(t1.a+t1.b+t1.c);
-        System.out.println(t2.a+t2.b+t2.c);
-
-
-        
+        System.out.println(t2.a+t2.b+t2.c);        
     }
 }
+
+class Account{
+    int acc_id;
+    String acc_name;
+    double acc_bal;
+    static double min_bal;
+    static String branch_name="SBI";
+
+    public static void main(String[] args) {
+        Account a1=new Account();
+        Account a2=new Account();
+        Account a3=new Account();
+        a1.acc_id=101;
+        a1.acc_name="balu";
+        
+
+        System.out.println(a1.acc_id);
+        System.out.println(a1.acc_name);
+
+
+    }
+}
+
+class G{
+    int a=100;
+    static int b=290;
+    
+    public static void main(String[] args) {
+        int c=300;
+
+        G g1=new G();
+        //instance variable
+        System.out.println(g1.a);//100
+        //static varibale example
+        System.out.println(G.b);//using class name
+        System.out.println(g1.b);//using object name
+        System.out.println(b);//using directly
+        System.out.println(c);
+        System.out.println(g1.a);
+    }
+} 
+ class H{
+    public static void main(String[] args) {
+        int a=100;
+    }
+    
+ }
+
+ 
 
