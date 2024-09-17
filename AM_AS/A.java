4eb01987ab7ddbd5ee8094649b0a7f3752bbd4d3
Balu9911 <sbalu2401@gmail.com>
2024-09-17 13:34:51 +0530

    class

---
+++ AM_AS/A.java
@@ -0,0 +1,20 @@
+package pack1;
+public class A{
+    public void m1(){
+        System.out.println("balu");
+        
+    }
+           void m2(){
+        System.out.println("junior");
+            
+           }
+    protected void m3(){
+        System.out.println("rohi");
+
+    }
+    private void m4(){
+        System.out.println("rohi");
+
+    }       
+
+}
---
+++ AM_AS/B.java
@@ -0,0 +1,7 @@
+package pack1;
+class B{
+    public void m5(){
+        System.out.println("pavan");
+    }
+     
+}
