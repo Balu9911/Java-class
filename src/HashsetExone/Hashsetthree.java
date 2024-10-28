76817d87e23432dbcaa9e9a00052f3f63c984fcd
Balu9911 <sbalu2401@gmail.com>
2024-10-28 20:29:31 +0530

    "class"

--- src/HashsetExone/Hashsetthree.java
+++ src/HashsetExone/Hashsetthree.java
@@ -10,8 +10,23 @@ public class Hashsetthree {
         s1.add(3);
         s1.add(4);
         HashSet<Integer> s2=new HashSet<Integer>();
+        s2.add(3);
         s2.add(4);
         s2.add(5);
         s2.add(6);
+        //set operations
+        //Union opeearations
+//        s1.addAll(s2);
+//        System.out.println(s1);
+        //Intersection Operator
+//        s1.retainAll(s2);
+//        System.out.println(s1);
+        //Only set 1-elements
+//        s1.removeAll(s2);
+//        System.out.println(s1);//[1,2]
+//        System.out.println(s2);//[3,4,5,6] for set-2
+        s1.containsAll(s2);
+        System.out.println(s1);//[1,2,3,4]
+        System.out.println(s2);//[3,4,5,6]
     }
 }
